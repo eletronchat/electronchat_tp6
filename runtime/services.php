005bfd55ee0deb35c54395c23c52fae401df4a02
wuchuheng <wuchuheng@163.com>
2019-07-02 01:45:59 -0700

    it was default to close the menu side

--- runtime/services.php
+++ runtime/services.php
@@ -1,5 +1,5 @@
 <?php 
-// This cache file is automatically generated at:2019-07-01 01:22:37
+// This cache file is automatically generated at:2019-07-02 16:27:06
 declare (strict_types = 1);
 return array (
   0 => 'think\\captcha\\CaptchaService',
