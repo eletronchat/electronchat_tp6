663f46860e0e172adf04b3a04584a23ec9897a07
wuchuheng <wuchuheng@163.com>
2019-07-10 17:16:16 -0700

    add the enter event  in  input area

--- public/chat/controller/chat.js
+++ public/chat/controller/chat.js
@@ -16,21 +16,7 @@
         element.render();
         element.render('collapse');
         layedit = layui.layedit;
-        post.inbox({
-          from: 'chat',  
-          onMessage: function(data) {
-            console.log(data); 
-          }
-        });
-
-        post.sent({
-          data: {
-            value: '我是chat页面', // 发件的内容
-            to: 'websocket' // 接收人  
-          }
-        });
-        
-    layedit.build('input-area', {
+    var index = layedit.build('input-area', {
      height : 180,
      tool: [
        'face' //表情
@@ -40,11 +26,27 @@
        ,'help' //帮助
      ]
     }); 
+    post.inbox({
+      from: '/brower/chat/index/message',  
+      onMessage: function(data) {
+        //接收聊天输入回车信号
+        console.log(data.value);
+        if (data.value.is_enter) {
+            console.log(layedit.getText(index));
+        }
+      }
+    });
+    //post.sent({
+    //  data: {
+    //    value: '我是chat页面', // 发件的内容
+    //    to: 'websocket' // 接收人  
+    //  }
+    //});
+        
     form.render(); 
-    $(document).ready(function(){
-      //滚动条到最底下
+    layedit.sync(index); 
+    layer.ready(function(){
       var objDiv = document.getElementById('chat-log')
-      console.log(objDiv);
       objDiv.scrollTop = objDiv.scrollHeight;
     });
   });
