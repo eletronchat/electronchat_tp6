918f28ab0b18702353db1b19c7aa3b5f9bdb5793
wuchuheng <wuchuheng@163.com>
2019-07-22 09:50:16 -0700

    update

--- app/chat/Events.php
+++ app/chat/Events.php
@@ -38,43 +38,44 @@ class Events
 {
     public static function onWebSocketConnect($client_id, $data)
     {
-      //根据路由进行连接验证 
-      switch(parse_url($data['server']['REQUEST_URI'])['path']) {
-           //客户连接 
-           case '/service/guest/login':
-             if(Guest::isServerOnline()) {
-                 Guest::cacheGuestData($client_id, $data); //缓存客户信息
-                Guest::welcome($client_id); //将客户拉入空闲座席
-             } else {
+        //根据路由进行连接验证 
+        switch(parse_url($data['server']['REQUEST_URI'])['path']) {
+            //客户连接 
+        case '/service/guest/login':
+            Guest::bindUid($client_id, $data);
+            if(Guest::isServerOnline()) {
+                Guest::cacheGuestData($client_id, $data); //缓存客户信息
+                Guest::welcome($client_id);                //将客户拉入空闲座席
+            } else {
                 // 客服不在线处理 
                 Guest::cacheToMissingSet($client_id, $data);
                 Gateway::sendToClient($client_id, json_encode(array(
                     'type' => 'miss',
                     'time' => microtime(true)
                 ))); 
-             }
-           break;
+            }
+            break;
 
-           //控制台连接
-           case '/service/admin/login' :
-                 // ...
-           break;
+            //控制台连接
+        case '/service/admin/login' :
+            // ...
+            break;
 
-           //客服工作台连接
-           case '/service/chat/login' :
-             //连接验证
-             if (!Check::chatConnect($data)) {
-               Check::sendMessageAndCloseByClientId($client_id);
-             } else {
-              //初始化连接
-              Chat::initConnect($data['get']['access-token'], $client_id);
-             }
-           break;
-           // 断开其余非法连接
-           default: 
-             var_dump($data['server']['REQUEST_URI']);
-             Gateway::closeClient($client_id);
-      }
+            //客服工作台连接
+        case '/service/chat/login' :
+            //连接验证
+            if (!Check::chatConnect($data)) {
+                Check::sendMessageAndCloseByClientId($client_id);
+            } else {
+                //初始化连接
+                Chat::initConnect($data['get']['access-token'], $client_id);
+            }
+            break;
+            // 断开其余非法连接
+        default: 
+            var_dump($data['server']['REQUEST_URI']);
+            Gateway::closeClient($client_id);
+        }
     }
 
     /**
@@ -89,42 +90,42 @@ class Events
         //Gateway::sendToClient($client_id, "Hello $client_id\r\n");
         // 向所有人发送
         //Gateway::sendToAll("$client_id login\r\n");
-    }
-    
-   /**
-    * 当客户端发来消息时触发
-    * @param int $client_id 连接id
-    * @param mixed $message 具体消息
-    */
-   public static function onMessage($client_id, $message)
-   {
-        //var_dump($client_id);
-        // 向所有人发送 
-        //Gateway::sendToAll("$client_id said $message\r\n");
-   }
-   
-   /**
-    * 当用户断开连接时触发
-    * @param int $client_id 连接id
-    */
-   public static function onClose($client_id)
-   {
-     // 客服设备中断处理
-     if (Chat::isChatConnect($client_id)) {
+}
+
+/**
+ * 当客户端发来消息时触发
+ * @param int $client_id 连接id
+ * @param mixed $message 具体消息
+ */
+public static function onMessage($client_id, $message)
+{
+    //var_dump($client_id);
+    // 向所有人发送 
+    //Gateway::sendToAll("$client_id said $message\r\n");
+}
+
+/**
+ * 当用户断开连接时触发
+ * @param int $client_id 连接id
+ */
+public static function onClose($client_id)
+{
+    // 客服设备中断处理
+    if (Chat::isChatConnect($client_id)) {
         $uid = Chat::getUidByCientId($client_id);
         Chat::delConnectCacheByCId($client_id);
         if (count(Gateway::getClientIdByUid($uid)) === 0) {
-          //当前账号所有设备都下线了，撤回挂牌在线
-          Chat::withdrawOnlineByUid($uid); 
-          // 撤回空闲座席
-          Chat::withdrawChatWaitingGroup($uid);
-          // .. 其它处理， 当前客户转移，或者等待10s,还是不能转移就向客户道歉原因
-        }
-     }
-     //客服连接中断处理 
-     if (Guest::isGuestConnect($client_id)) {
-         Guest::delConnectCacheByCId($client_id);
-     }
+            //当前账号所有设备都下线了，撤回挂牌在线
+            Chat::withdrawOnlineByUid($uid); 
+            // 撤回空闲座席
+            Chat::withdrawChatWaitingGroup($uid);
+            // .. 其它处理， 当前客户转移，或者等待10s,还是不能转移就向客户道歉原因
+}
+}
+//客服连接中断处理 
+if (Guest::isGuestConnect($client_id)) {
+    Guest::delConnectCacheByCId($client_id);
+}
 
-   }
+}
 }
